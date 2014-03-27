b259aa4aab3a06af25625dcec9f9369e41425dfc
yayanyang <yayanyang@gmail.com>
2014-03-27 10:46:26 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- lemon-runtimes/build.sbt
+++
@@ -1,1 +0,0 @@
-import lemon.compilers._
--- lemon-runtimes/project/LemonRuntimesBuild.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-import sbt.Keys._
-import lemon.compilers._
-
-object LemonRuntimesBuild extends Build {
-//  libraryDependencies += "lemon" % "lemon-compilers"
-
-}
