f3957d6c78b886242900cb5bb22b91127bbfdea5
yayanyang <yayanyang@gmail.com>
2014-03-29 19:08:37 +0800

    update

--- lemon-compilers/src/main/scala/lemon/compilers/backend/JavaRWCodeGen.scala
+++ lemon-compilers/src/main/scala/lemon/compilers/backend/JavaRWCodeGen.scala
@@ -4,6 +4,7 @@ import lemon.compilers.frontend._
 import com.sun.codemodel._
 import lemon.compilers.frontend.FieldIL
 import lemon.compilers.frontend.MessageIL
+import lemon.messages.CollectionWriter
 
 trait JavaRWCodeGen extends JavaBackend{
 
@@ -52,7 +53,7 @@ trait JavaRWCodeGen extends JavaBackend{
   }
 
   def writeField(field:FieldIL,writer: JVar,resolver:JVar,method:JMethod,model:JDefinedClass) {
-    val invoke = field.fieldType match {
+    field.fieldType match {
       case VarIL(length,signed) =>
         method.body()
           .invoke(writer,"writeVar")
@@ -60,6 +61,7 @@ trait JavaRWCodeGen extends JavaBackend{
           .arg(JExpr.lit(field.id))
           .arg(JExpr.lit(length))
           .arg(JExpr.lit(signed))
+          .arg(JExpr.ref(field.name))
           .arg(resolver.invoke("resolve").arg(JExpr.lit(model.fullName() + "#" + field.name)))
       case FixedIL(length,signed) =>
         method.body()
@@ -68,12 +70,16 @@ trait JavaRWCodeGen extends JavaBackend{
           .arg(JExpr.lit(field.id))
           .arg(JExpr.lit(length))
           .arg(JExpr.lit(signed))
+          .arg(JExpr.ref(field.name))
+          .arg(resolver.invoke("resolve").arg(JExpr.lit(model.fullName() + "#" + field.name)))
 
       case string:StringIL =>
         method.body()
           .invoke(writer,"writeString")
           .arg(JExpr.lit(field.name))
           .arg(JExpr.lit(field.id))
+          .arg(JExpr.ref(field.name))
+          .arg(resolver.invoke("resolve").arg(JExpr.lit(model.fullName() + "#" + field.name)))
 
       case FloatIL(length) =>
         length match{
@@ -82,25 +88,49 @@ trait JavaRWCodeGen extends JavaBackend{
               .invoke(writer,"writeFloat")
               .arg(JExpr.lit(field.name))
               .arg(JExpr.lit(field.id))
+              .arg(JExpr.ref(field.name))
+              .arg(resolver.invoke("resolve").arg(JExpr.lit(model.fullName() + "#" + field.name)))
           case 8 =>
             method.body()
               .invoke(writer,"writeDouble")
               .arg(JExpr.lit(field.name))
               .arg(JExpr.lit(field.id))
+              .arg(JExpr.ref(field.name))
+              .arg(resolver.invoke("resolve").arg(JExpr.lit(model.fullName() + "#" + field.name)))
         }
-
       case ListIL(valType) =>
-        method.body().forEach(getJType(valType),"current",JExpr.ref(field.name))
-        method.body().invoke(writer,"createListWriter")
+        val collectionWriter = method.body().decl(
+          codeModel.ref(classOf[CollectionWriter]),
+          field.name + "Writer",
+          method.body().invoke(writer,"createListWriter")
+            .arg(JExpr.ref(field.name))
+            .arg(resolver.invoke("resolve")
+            .arg(JExpr.lit(model.fullName() + "#" + field.name))))
+
+
+        val foreach = method.body().forEach(getJType(valType),"current",JExpr.ref(field.name))
+
+        foreach.body().invoke(collectionWriter,"writeNext")
+
+        writeCollectionType(collectionWriter,foreach.body(),foreach.`var`(),valType)
+
       case ArrayIL(valType,length) =>
         method.body().forEach(getJType(valType),"current",JExpr.ref(field.name))
         method.body().invoke(writer,"createArrayWriter")
       case MapIL(keyType,valType) =>
         method.body().invoke(writer,"createMapWriter")
     }
+  }
 
-    invoke
-      .arg(JExpr.ref(field.name))
-      .arg(resolver.invoke("resolve").arg(JExpr.lit(model.fullName() + "#" + field.name)))
+  private def writeCollectionType(collectionWriter:JVar,block:JBlock,value:JVar,valType:TypeIL){
+    valType match {
+      case VarIL(length,signed) =>
+        block.invoke(collectionWriter,"writeVar")
+          .arg(value)
+          .arg(JExpr.lit(length))
+          .arg(JExpr.lit(signed))
+      case string:StringIL =>
+        block.invoke(collectionWriter,"writeString").arg(value)
+    }
   }
 }
